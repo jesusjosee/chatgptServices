dd58ae5edb768f205495655dda5a006362dea804
jesus <suyon.jesus@gmail.com>
2023-06-01 20:27:35 -0500

    Tests edited

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -1,4 +1,6 @@
-from django.test import TestCase, Client
+import os
+from rest_framework.test import APIClient
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -7,7 +9,7 @@ from decouple import config
 
 class APiKeyAutenticationViewTest(TestCase):
     def setUp(self):
-        self.client = Client()
+        self.client = APIClient()
 
     def test_valid_api_key(self):
         api_key = config('OPENAI_API_KEY')
@@ -32,12 +34,14 @@ class APiKeyAutenticationViewTest(TestCase):
         self.assertEqual(response.data, {'response': 'invalid_api_key'})
 
 
-class UploadCSVAPIViewTest(APITestCase):
-    def test_valid_csv_file(self):
-        csv_file = open('static\csv\data_test.csv', 'rb')
-        response = self.client.post('/api/uploadcsv', {'file': csv_file}, format='multipart')
 
-        expected_processed_data = [
+class UploadCSVAPIViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.csv_file_path = os.path.join('static', 'csv', 'data_test.csv')
+        self.invalid_file_path = os.path.join('static', 'csv', 'invalid_test.txt')
+        self.malformed_file_path = os.path.join('static', 'csv', 'malformed_test.csv')
+        self.expected_processed_data = [
             ['jesus', 'suyon', 29, 'yes'],
             ['chari', 'soplapuco', 26, 'yes'],
             ['carlos', 'sosa', 26, 'no'],
@@ -45,8 +49,13 @@ class UploadCSVAPIViewTest(APITestCase):
             ['maria', 'dominguez', 43, 'yes'],
         ]
 
+    def test_valid_csv_file(self):
+        csv_file = open(self.csv_file_path, 'rb')
+        response = self.client.post('/api/uploadcsv', {'file': csv_file}, format='multipart')
+        csv_file.close()
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['data'], expected_processed_data)
+        self.assertEqual(response.data['data'], self.expected_processed_data)
 
     def test_missing_file(self):
         response = self.client.post('/api/uploadcsv', {}, format='multipart')
@@ -55,15 +64,17 @@ class UploadCSVAPIViewTest(APITestCase):
         self.assertEqual(response.data['error'], 'No file provided')
 
     def test_invalid_file_format(self):
-        invalid_file = open('static\csv\invalid_test.txt', 'rb')
+        invalid_file = open(self.invalid_file_path, 'rb')
         response = self.client.post('/api/uploadcsv', {'file': invalid_file}, format='multipart')
+        invalid_file.close()
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['error'], 'The file must be in CSV format')
-   
+
     def test_malformed_data(self):
-        malformed_file = open('static\csv\malformed_test.csv', 'rb')
+        malformed_file = open(self.malformed_file_path, 'rb')
         response = self.client.post('/api/uploadcsv', {'file': malformed_file}, format='multipart')
+        malformed_file.close()
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['error'], 'The data is malformed or missing data in the rows.')
